f6298d6098558463a89ee173fe80586cbcf54f2b
Vishwa Prabashwara <127642671+Rocky011015@users.noreply.github.com>
2026-08-21 00:02:23 +0530

    chore: initialize Spring Boot project

---
+++ src/main/java/com/sunrisedental/clinic/SunriseDentalClinicApplication.java
@@ -0,0 +1,13 @@
+package com.sunrisedental.clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunriseDentalClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunriseDentalClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sunrisedental/clinic/SunriseDentalClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunrisedental.clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunriseDentalClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
